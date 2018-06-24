ee6caebeeebbb014e7cb9bcbbd3d98b24101f7b0
EdnilsonRobert <ednilsonrobert@gmail.com>
2018-06-23 21:04:45 -0300

    Adicionando BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+      browsersync = require('browser-sync');
+
+const PATH_SRC = './src/';
+
+// Criar servidor e sincronizar navegador
+gulp.task('browser-dev', function() {
+    browsersync.init({
+        server: {
+            baseDir: PATH_SRC,
+            index: './index.html'
+        },
+        port: 3000
+    });
+});
 
 gulp.task('default', function() {
     console.info('>>> Gulp funfando de boas...');
